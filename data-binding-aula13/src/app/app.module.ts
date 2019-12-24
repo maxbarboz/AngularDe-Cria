05d2eeb0b8c982cd4d4ac1384c38e2a9c8d48746
Maxsuel Storch <maxsuelstorch@gmail.com>
2019-12-24 13:28:26 -0300

    Input Properties

---
+++ data-binding-aula13/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { InputPropertyComponent } from './input-property/input-property.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBindingComponent,
+    InputPropertyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
